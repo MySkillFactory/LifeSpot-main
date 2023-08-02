16fdce372fccc14051a38281b57684379b585668
6eh <mcls@list.ru>
2023-08-02 20:41:44 +0200

    2
    2

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,8 @@
+﻿let age = prompt("Введите ваш возраст");
+if (age >= 18) {
+    alert('Приветствуем на LifeSpot ' + new Date().toLocaleString());
+    //console.log(age);
+} else {
+    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+    window.location.href = "http://www.google.com"
+}
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,5 @@
+﻿/*let name = prompt("Введите ваше имя:");
+alert(`Ваше имя ${name}. В вашем имени ${name.length} символов`);
+*/
+let elements = document.getElementsByTagName("*");
+alert(`На странице элементов: ${elements.length}`)
